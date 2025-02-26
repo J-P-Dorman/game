810b1423457fc1819f1c7b9f05dfd43fa2baf141
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-02-26 13:29:43 +0000

    Fix item types

--- src/views/Game/components/WorldMap/components/Item/types.ts
+++ src/views/Game/components/WorldMap/components/Item/types.ts
@@ -10,15 +10,13 @@ export type ItemData = {
     defaultSprite: string; // Name of sprite to display by default
     defaultAnimation?: string; // Name of animation to play by default
     sheetMap: string[][]; // Assign ids to each sprite based on sheet position, first array is the sheet, second array is the row
-    animationMap: {
-      water: {
-        // Animation name
-        frames: string[]; // Chronological ids of frames to grab from the spritesheet for the animation
-        fps: 3; // Frames to show per second, controls animation speed
-        loop: true; // Should animation infinitely loop
-        end?: string; // What frame to display when the animation ends
-      };
-    };
+    animationMap: Record<string, {
+      // Animation name
+      frames: string[]; // Chronological ids of frames to grab from the spritesheet for the animation
+      fps: number; // Frames to show per second, controls animation speed
+      loop: boolean; // Should animation infinitely loop
+      end?: string; // What frame to display when the animation ends
+    }>;
   };
   onInteract: () => void; // What to do when the player interacts with the object
 };
