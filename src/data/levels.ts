456b8ab4eba11953e13f9d45c8fb886d1ca17450
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-02-27 12:55:12 +0000

    Add some more items to map

--- src/data/levels.ts
+++ src/data/levels.ts
@@ -1,11 +1,18 @@
-import { bucketData } from "../views/Game/components/WorldMap/components/Item/itemData";
+import { bucketData, houseLightData, treeData } from "../views/Game/components/WorldMap/components/Item/itemData";
 import { dialogueOptionsSasha, sashaData } from "./creatures/sasha";
 import { mapTutorialIsland } from "./mapData";
 
 const tutorialIsland = {
   playerStart: [40, -42],
   map: mapTutorialIsland,
-  items: [{ id: "testBucket", position: [41, 41], ...bucketData }],
+  items: [
+    { id: "house", position: [32, 38], ...houseLightData },
+    { id: "house2", position: [44, 38], ...houseLightData },
+    { id: "house3", position: [32, 31], ...houseLightData },
+    { id: "house4", position: [44, 31], ...houseLightData },
+    { id: "tree", position: [28, 34], ...treeData },
+    { id: "testBucket", position: [39, 42], ...bucketData }
+  ],
   creatures: [
     {
       id: "sasha",
--- src/views/Game/components/WorldMap/components/Item/itemData.ts
+++ src/views/Game/components/WorldMap/components/Item/itemData.ts
@@ -1,9 +1,11 @@
 import bucketSpriteSheet from "../../../../../../assets/sprites/Items/bucket.svg";
+import houseLightSpriteSheet from "../../../../../../assets/sprites/Items/house_light.svg";
+import treeSpriteSheet from "../../../../../../assets/sprites/Items/tree_1.svg";
+import bedHorizontalSpriteSheet from "../../../../../../assets/sprites/Items/bed_horizontal.svg";
 import { ItemData } from "./types";
 
 export const bucketData: ItemData = {
-  width: 0.5,
-  height: 0.5,
+  size: 0.7,
   spriteSheet: {
     image: bucketSpriteSheet,
     spriteWidth: 14,
@@ -24,5 +26,78 @@ export const bucketData: ItemData = {
   },
   onInteract: () => {
     alert("DON'T TOUCH THE BUCKET");
+  },
+};
+
+export const houseLightData: ItemData = {
+  size: 5,
+  spriteSheet: {
+    image: houseLightSpriteSheet,
+    spriteWidth: 97,
+    spriteHeight: 65,
+    sheetWidth: 97,
+    sheetHeight: 65,
+    defaultSprite: "water1",
+    defaultAnimation: "water",
+    sheetMap: [["water1"]],
+    animationMap: {
+      water: {
+        frames: ["water1"],
+        fps: 1,
+        loop: false,
+        end: undefined
+      },
+    },
+  },
+  onInteract: () => {
+    alert("DON'T TOUCH THE HOUSE");
+  },
+};
+
+export const treeData: ItemData = {
+  size: 4,
+  spriteSheet: {
+    image: treeSpriteSheet,
+    spriteWidth: 32,
+    spriteHeight: 45,
+    sheetWidth: 32,
+    sheetHeight: 45,
+    defaultSprite: "tree",
+    defaultAnimation: "tree",
+    sheetMap: [["tree"]],
+    animationMap: {
+      tree: {
+        frames: ["tree"],
+        fps: 1,
+        loop: false
+      },
+    },
+  },
+  onInteract: () => {
+    alert("DON'T TOUCH THE TREE");
+  },
+};
+
+export const bedHorizontalData: ItemData = {
+  size: 2,
+  spriteSheet: {
+    image: bedHorizontalSpriteSheet,
+    spriteWidth: 49,
+    spriteHeight: 49,
+    sheetWidth: 28,
+    sheetHeight: 28,
+    defaultSprite: "bedHoriz",
+    defaultAnimation: "bedHoriz",
+    sheetMap: [["bedHoriz"]],
+    animationMap: {
+      bedHoriz: {
+        frames: ["bedHoriz"],
+        fps: 1,
+        loop: false
+      },
+    },
+  },
+  onInteract: () => {
+    alert("DON'T TOUCH THE BED");
   },
 };
