53edca191919e950ea1c2aeddcee5110fe715606
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-02-28 15:14:06 +0000

    Move houses
    Move houses around in tutorial level

--- src/data/levels.ts
+++ src/data/levels.ts
@@ -6,10 +6,10 @@ const tutorialIsland = {
   playerStart: [40, -42],
   map: mapTutorialIsland,
   items: [
-    { id: "house", position: [32, 38], ...houseLightData },
-    { id: "house2", position: [44, 38], ...houseLightData },
-    { id: "house3", position: [32, 31], ...houseLightData },
-    { id: "house4", position: [44, 31], ...houseLightData },
+    { id: "house", position: [33, 38], ...houseLightData },
+    { id: "house2", position: [43, 38], ...houseLightData },
+    { id: "house3", position: [33, 31], ...houseLightData },
+    { id: "house4", position: [43, 31], ...houseLightData },
     { id: "tree", position: [28, 34], ...treeData },
     { id: "testBucket", position: [39, 42], ...bucketData }
   ],
